b8819e93850358cd185951256e42442e68257c1d
Tmanbear <tyler.s.holewinski@gmail.com>
2017-09-19 18:38:10 -0700

    Update pic_inject.js

--- pic_inject.js
+++ pic_inject.js
@@ -1,6 +1,6 @@
 var changePictureTo = function(pictureAddr) {
 	var targets = $("img");
-	targets.attr("css", pictureAddr);
+	targets.attr("src", pictureAddr);
 	console.log("done");
 	console.log("updated all img tag href attributes to " + pictureAddr);
 }
